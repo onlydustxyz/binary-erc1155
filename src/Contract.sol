735668e2b6114ea5d058341ca184fdabf8f4ca21
Stanislas Bernard <bernardstanislas@gmail.com>
2022-03-31 18:06:18 +0200

    style(contracts): comply to linting rules

--- src/Contract.sol
+++ src/Contract.sol
@@ -8,12 +8,13 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
  * See https://eips.ethereum.org/EIPS/eip-1155
  * Originally based on code by OpenZeppelin: https://github.com/OpenZeppelin/openzeppelin-contracts
  *
- * This implementation lets addresses hold a unique instance of multiple tokens, but they cannot have more than one token id
+ * This implementation lets addresses hold a unique instance of multiple tokens,
+ * but they cannot have more than one token id
  */
 contract BinaryERC1155 is ERC1155 {
-
     /**
      * @dev See {_setURI}.
      */
+    // solhint-disable no-empty-blocks
     constructor(string memory uri_) ERC1155(uri_) {}
 }
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.10 <0.9.0;
 import "ds-test/test.sol";
 
 contract ContractTest is DSTest {
+    // solhint-disable no-empty-blocks
     function setUp() public {}
 
     function testExample() public {
