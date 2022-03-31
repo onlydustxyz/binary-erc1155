d0452e84be54cfbadb0be00715383144156ba87e
Stanislas Bernard <bernardstanislas@gmail.com>
2022-03-31 17:51:25 +0200

    feat: bootstrap project configuration

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.10 <0.9.0;
 
-contract Contract {}
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+/**
+ * @dev Implementation of a binary multi-token.
+ * See https://eips.ethereum.org/EIPS/eip-1155
+ * Originally based on code by OpenZeppelin: https://github.com/OpenZeppelin/openzeppelin-contracts
+ *
+ * This implementation lets addresses hold a unique instance of multiple tokens, but they cannot have more than one token id
+ */
+contract BinaryERC1155 is ERC1155 {
+
+    /**
+     * @dev See {_setURI}.
+     */
+    constructor(string memory uri_) ERC1155(uri_) {}
+}
