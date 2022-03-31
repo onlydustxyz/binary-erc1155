a2da3e7f18ffe7c47106828ddb603a9bba2eb044
Stanislas Bernard <bernardstanislas@gmail.com>
2022-03-31 18:51:39 +0200

    feat(contract): unpack uint256

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,12 +2,25 @@
 pragma solidity >=0.8.10 <0.9.0;
 
 import "ds-test/test.sol";
+import "./SystemUnderTest.sol";
 
 contract ContractTest is DSTest {
-    // solhint-disable no-empty-blocks
-    function setUp() public {}
+    SystemUnderTest private _sut;
 
-    function testExample() public {
-        assertTrue(true);
+    // solhint-disable no-empty-blocks
+    function setUp() public {
+        _sut = new SystemUnderTest("");
+    }
+
+    function testUnpacking() public {
+        uint256 packedNumber = 1356;
+        uint256[] memory unpackedNumber = _sut.unpackNumber(packedNumber);
+
+        assertEq(unpackedNumber.length, 5);
+        assertEq(unpackedNumber[0], 2);
+        assertEq(unpackedNumber[1], 3);
+        assertEq(unpackedNumber[2], 6);
+        assertEq(unpackedNumber[3], 8);
+        assertEq(unpackedNumber[4], 10);
     }
 }
