9fa42d5b88d2bc9cdcec0597b9aa2d1129b30e93
Stanislas Bernard <bernardstanislas@gmail.com>
2022-04-01 19:24:58 +0200

    feat: mask matching method

--- src/BitOperation.sol
+++ src/BitOperation.sol
@@ -5,15 +5,19 @@ pragma solidity >=0.8.10 <0.9.0;
  * Bit manipulation made easy on 32 bytes slots represented by bytes32 primitives
  */
 library BitOperation {
-    function getBit(uint256 input, uint8 index) external pure returns (bool) {
-        return (input & (1 << index)) != 0;
+    function getBit(uint256 input_, uint8 index_) external pure returns (bool) {
+        return (input_ & (1 << index_)) != 0;
     }
 
-    function clearBit(uint256 input, uint8 index) external pure returns (uint256) {
-        return input & ~(1 << index);
+    function clearBit(uint256 input_, uint8 index_) external pure returns (uint256) {
+        return input_ & ~(1 << index_);
     }
 
-    function setBit(uint256 input, uint8 index) external pure returns (uint256) {
-        return input | (1 << index);
+    function setBit(uint256 input_, uint8 index_) external pure returns (uint256) {
+        return input_ | (1 << index_);
+    }
+
+    function matchesMask(uint256 input_, uint256 mask_) external pure returns (bool) {
+        return (input_ & mask_) == mask_;
     }
 }
