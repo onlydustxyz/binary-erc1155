5cced6646267bd7e1d066b35b3abd2e5e0d1e1a1
Stanislas Bernard <bernardstanislas@gmail.com>
2022-04-01 17:28:40 +0200

    feat: bit operation library

---
+++ src/BitOperation.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10 <0.9.0;
+
+/**
+ * Bit manipulation made easy on 32 bytes slots represented by bytes32 primitives
+ */
+library BitOperation {
+    function getBit(uint256 input, uint8 index) external pure returns (bool) {
+        return (input & (1 << index)) != 0;
+    }
+
+    function clearBit(uint256 input, uint8 index) external pure returns (uint256) {
+        return input & ~(1 << index);
+    }
+}
