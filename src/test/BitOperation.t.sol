626db80ad85934b4c56a350807613fda3f5d0b5e
Stanislas Bernard <bernardstanislas@gmail.com>
2022-04-01 17:28:40 +0200

    feat: bit operation library

---
+++ src/test/BitOperation.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10 <0.9.0;
+
+import "ds-test/test.sol";
+import "../BitOperation.sol";
+
+interface CheatCodes {
+    function assume(bool) external;
+}
+
+contract BitOperationTest is DSTest {
+    using BitOperation for uint256;
+
+    CheatCodes private cheats = CheatCodes(HEVM_ADDRESS);
+
+    function testGetBit() public {
+        uint256 oddNumber = 127;
+        assertTrue(oddNumber.getBit(0));
+
+        uint256 evenNumber = 128;
+        assertTrue(!evenNumber.getBit(0));
+
+        uint256 bigNumber = 1567;
+        assertTrue(bigNumber.getBit(0));
+        assertTrue(!bigNumber.getBit(5));
+        assertTrue(bigNumber.getBit(9));
+    }
+
+    function testClearBit(uint256 input_, uint8 bitIndex_) public {
+        uint256 clearedNumber = input_.clearBit(bitIndex_);
+        if (input_.getBit(bitIndex_)) {
+            assertEq(clearedNumber, input_ - 2**bitIndex_);
+        } else {
+            assertEq(clearedNumber, input_);
+        }
+    }
+}
