cd4a0d4e4f3d407535c2e0746fe620becba670d0
Stanislas Bernard <bernardstanislas@gmail.com>
2022-04-12 18:31:18 +0200

    fix(BitOperation): make all functions internal

--- contracts/BitOperation.sol
+++ contracts/BitOperation.sol
@@ -5,23 +5,23 @@ pragma solidity >=0.8.10 <0.9.0;
  * Bit manipulation made easy on 32 bytes slots represented by bytes32 primitives
  */
 library BitOperation {
-    function getBit(uint256 input_, uint8 index_) external pure returns (bool) {
+    function getBit(uint256 input_, uint8 index_) internal pure returns (bool) {
         return (input_ & (1 << index_)) != 0;
     }
 
-    function clearBit(uint256 input_, uint8 index_) external pure returns (uint256) {
+    function clearBit(uint256 input_, uint8 index_) internal pure returns (uint256) {
         return input_ & ~(1 << index_);
     }
 
-    function setBit(uint256 input_, uint8 index_) external pure returns (uint256) {
+    function setBit(uint256 input_, uint8 index_) internal pure returns (uint256) {
         return input_ | (1 << index_);
     }
 
-    function matchesMask(uint256 input_, uint256 mask_) external pure returns (bool) {
+    function matchesMask(uint256 input_, uint256 mask_) internal pure returns (bool) {
         return (input_ & mask_) == mask_;
     }
 
-    function negatesMask(uint256 input_, uint256 mask_) external pure returns (bool) {
+    function negatesMask(uint256 input_, uint256 mask_) internal pure returns (bool) {
         return (input_ & mask_) == 0;
     }
 
